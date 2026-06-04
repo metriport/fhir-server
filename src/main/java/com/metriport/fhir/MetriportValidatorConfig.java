62dacf8033f045878d73124e4b5d18e6548c2cb2
jwar44 <jacob@metriport.com>
2026-06-04 13:18:50 -0700

    fix(config): add logs
    refs. eng#2761
    
    Signed-off-by: jwar44 <jacob@metriport.com>

--- src/main/java/com/metriport/fhir/MetriportValidatorConfig.java
+++ src/main/java/com/metriport/fhir/MetriportValidatorConfig.java
@@ -1,7 +1,12 @@
 package com.metriport.fhir;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
 import org.hl7.fhir.r5.utils.validation.constants.BestPracticeWarningLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,24 +21,38 @@ import ca.uhn.fhir.jpa.starter.annotations.OnR4Condition;
 @Conditional(OnR4Condition.class)
 public class MetriportValidatorConfig {
 
+	private static final Logger log = LoggerFactory.getLogger(MetriportValidatorConfig.class);
+
 	@Bean
 	static BeanPostProcessor instanceValidatorCustomizer() {
-		return new BeanPostProcessor() {
-			@Override
-			public Object postProcessAfterInitialization(Object bean, String beanName) {
-				if (!"myInstanceValidator".equals(beanName) || !(bean instanceof FhirInstanceValidator)) {
-					return bean;
-				}
-				FhirInstanceValidator validator = (FhirInstanceValidator) bean;
-				validator.setNoTerminologyChecks(true);
-				validator.setNoExtensibleWarnings(true);
-				validator.setBestPracticeWarningLevel(BestPracticeWarningLevel.Ignore);
-				validator.setErrorForUnknownProfiles(false);
-				
-				// Don't hit db for reference checks
-				validator.setAssumeValidRestReferences(true);
-				return validator;
+		return new InstanceValidatorCustomizer();
+	}
+
+	private static final class InstanceValidatorCustomizer implements BeanPostProcessor, SmartInitializingSingleton {
+
+		private final AtomicBoolean configured = new AtomicBoolean(false);
+
+		@Override
+		public Object postProcessAfterInitialization(Object bean, String beanName) {
+			if (!(bean instanceof FhirInstanceValidator)) {
+				return bean;
 			}
-		};
+			FhirInstanceValidator validator = (FhirInstanceValidator) bean;
+			validator.setNoTerminologyChecks(true);
+			validator.setNoExtensibleWarnings(true);
+			validator.setBestPracticeWarningLevel(BestPracticeWarningLevel.Ignore);
+			validator.setErrorForUnknownProfiles(false);
+			validator.setAssumeValidRestReferences(true); // Don't hit db for reference checks
+			configured.set(true);
+			log.info("Applied Metriport validation settings to FhirInstanceValidator bean '{}'", beanName);
+			return validator;
+		}
+
+		@Override
+		public void afterSingletonsInstantiated() {
+			if (!configured.get()) {
+				log.warn("No FhirInstanceValidator bean found; Metriport validation settings were not applied");
+			}
+		}
 	}
 }
